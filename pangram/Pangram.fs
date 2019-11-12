56561b9969b281c045e0e725220b92a23bae3647
Diego Orellana <djorellanab@correo.url.edu.gt>
2019-11-11 21:46:49 -0600

    pangram problem

---
+++ pangram/Pangram.fs
@@ -0,0 +1,15 @@
+﻿module Pangram
+
+let inline isInRange x = (x > 64 && x < 91 ) 
+let inline charToInt c = int c
+
+let isPangram (input: string): bool = 
+    let mutable setAlphabet = Set.empty
+    let length = input.Length - 1
+    for i = 0 to length do
+        let charInput = input.Chars i |> System.Char.ToUpper
+        if isInRange (charInput |> charToInt ) then
+            if not(setAlphabet.Contains charInput) then
+                setAlphabet  <- setAlphabet.Add(charInput)
+    
+    setAlphabet.Count = 26
