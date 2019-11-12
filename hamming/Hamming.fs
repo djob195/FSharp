0682da3cc6270f6ce4ce020f6a84622402c8049f
Diego Orellana <djorellanab@correo.url.edu.gt>
2019-11-11 21:40:59 -0600

    Hamming problem

---
+++ hamming/Hamming.fs
@@ -0,0 +1,12 @@
+﻿module Hamming
+
+let distance (strand1: string) (strand2: string): int option = 
+    if strand1.Length <> strand2.Length then
+        None
+    else
+        let mutable result = 0
+        let length = strand1.Length - 1 ;
+        for i = 0 to length do
+            if strand1.Chars i <> strand2.Chars i then
+                result <- result + 1        
+        Some result
